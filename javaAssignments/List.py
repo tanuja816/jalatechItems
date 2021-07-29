6278e05401d07f40e272a8c5cbd5731c2ab5b506
Tanuja Rana <tanujarana541@gmail.com>
2021-07-29 11:27:51 -0700

    sample python file

---
+++ javaAssignments/List.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+num = [25,12,36,95,14]
+num
+
+
+# In[5]:
+
+
+num[0] #it will print the  number corressponding to the index number
+
+
+# In[6]:
+
+
+num[6]  ##coz the list containing only 4 elemets its out of range
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+num[2:] # it ill start from 3 element as in list index of elements strts from 0  and print till end
+
+
+# In[7]:
+
+
+num[-1]
+
+
+# In[8]:
+
+
+names = [ 'tanu','manu','kanu' ]
+
+
+# In[9]:
+
+
+names
+
+
+# In[3]:
+
+
+value = [9.5 ,'tanu',25]    ##u can have different type of data
+
+
+# In[6]:
+
+
+## two list working together
+names = [ 'tanu','manu','kanu' ]
+num = [25,12,36,95,14]
+
+mil=[num,names]
+mil
+
+
+# In[13]:
+
+
+### function of list
+#1.append
+num = [25,12,36,95,14]
+num.append(45)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
